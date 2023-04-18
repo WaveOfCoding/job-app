edb898a2dc3d767773a138eb2ba17e8d1a3d8439
WaveOfCoding <waveformwstcrz@gmail.com>
2023-04-18 12:12:07 +0300

    feature: Add schemas Users

--- service/db/session.py
+++ service/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from typing import Generator
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
@@ -8,3 +8,11 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
