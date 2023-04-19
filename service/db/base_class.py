b44dbfcc4702f6f3e6fc36cfaf1dd558306591bc
WaveOfCoding <waveformwstcrz@gmail.com>
2023-04-19 14:38:55 +0300

    feature: Add pytest for create user

--- service/db/base_class.py
+++ service/db/base_class.py
@@ -1,6 +1,6 @@
 from typing import Any
-from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.ext.declarative import as_declarative
 
 
 @as_declarative()
