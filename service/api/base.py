242a5be87a68ca52993c40ece55910a211bfd101
WaveOfCoding <waveformwstcrz@gmail.com>
2023-04-19 14:56:03 +0300

    feature: Add pystest for test jobs

--- service/api/base.py
+++ service/api/base.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter
 
 from api.version1 import route_general_pages
 from api.version1 import route_users
+from api.version1 import route_jobs
 
 api_router = APIRouter()
 api_router.include_router(route_general_pages.general_pages_router, prefix="", tags=["general_pages"])
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
